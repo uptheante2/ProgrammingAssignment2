a0b2af462601bfef25796a967ef712a1229e8db7
uptheante2 <mirabile.det752@gmail.com>
2014-07-27 17:40:44 -0400

    Revert "cachematrix.R"
    This reverts commit 2bf244c56bdffaf07b09d34cff56832f885ad84d.

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,15 @@
-## The function makeCacheMatrix takes in a matrix 'x' and generates the funciton calls to cache the variable 
-## values when called.
+## Put comments here that give an overall description of what your
+## functions do
 
-## The code used is modified from the template vector example.  The output of makeCacheMatrix generates the 
-## function calls in a list format to be used in the cacheSolve function.  The 'set' varlables are cache 
-## and the 'get' variables are not.
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        
 
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setinv <- function(solve) m <<- solve
-        getinv <- function() m
-        list(set = set, get = get,
-             setinv = setinv,
-             getinv = getinv)
 }
 
 
-## This function evaluates the functions set up in makeCacheMatrix and returns either the cached value or
-## the newly solved value.  
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        
-        m <- x$getinv()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinv(m)
-        m
 }
