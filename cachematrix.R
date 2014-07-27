bb94ce045a7ddd0939861599fa288c530f6293b0
uptheante2 <mirabile.det752@gmail.com>
2014-07-27 17:56:28 -0400

    Programming Project 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##The makeCacheMatrix funciton brings in the matrix 'x' and stores the value in cache for the 'set' variables
+## the 'get' variables will not be stored in cache.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(mean) m <<- mean
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## the cacheSolve function takes in the results from makeCacheMatrix which has all the function calls to be
+## executed and determines if the value can be taken from cache and does so.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
